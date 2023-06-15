fc2ac020cbd9eee9fb58030e3b27937195382d68
Thiagosdep <thiago_sousap@hotmail.com>
2023-06-15 02:42:21 -0300

    update packages and create new routes

--- frontend/src/components/App/index.js
+++ frontend/src/components/App/index.js
@@ -1,21 +1,23 @@
 import { ThemeProvider } from 'styled-components';
+import { BrowserRouter } from 'react-router-dom';
+
 import GlobalStyles from '../../assets/styles/global';
 import defaultTheme from '../../assets/styles/themes/default';
-
 import Header from '../Header';
-
+import Routes from '../../Routes';
 import { Container } from './styles';
-import ContactsList from '../ContactsList';
 
 function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <GlobalStyles />
-      <Container>
-        <Header />
-        <ContactsList />
-      </Container>
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={defaultTheme}>
+        <GlobalStyles />
+        <Container>
+          <Header />
+          <Routes />
+        </Container>
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
