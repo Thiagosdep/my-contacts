0773adfafd7ec4b1b5a57294ff3f61efe3af2dd6
Thiagosdep <thiago_sousap@hotmail.com>
2023-06-15 02:18:57 -0300

    frontend: initial configuration

---
+++ frontend/src/components/App/index.js
@@ -0,0 +1,22 @@
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from '../../assets/styles/global';
+import defaultTheme from '../../assets/styles/themes/default';
+
+import Header from '../Header';
+
+import { Container } from './styles';
+import ContactsList from '../ContactsList';
+
+function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyles />
+      <Container>
+        <Header />
+        <ContactsList />
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+export default App;
