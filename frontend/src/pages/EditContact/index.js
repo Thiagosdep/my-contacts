34a682b43405cea4ea08c29128ba36e923c539ae
Thiagosdep <thiago_sousap@hotmail.com>
2023-06-15 03:11:31 -0300

    add PageHeader component

--- frontend/src/pages/EditContact/index.js
+++ frontend/src/pages/EditContact/index.js
@@ -1,3 +1,5 @@
+import PageHeader from '../../components/PageHeader';
+
 export default function EditContact() {
-  return 'editContact';
+  return <PageHeader title="Editar Thiago Sousa" />;
 }
--- frontend/src/pages/NewContact/index.js
+++ frontend/src/pages/NewContact/index.js
@@ -1,3 +1,5 @@
+import PageHeader from '../../components/PageHeader';
+
 export default function NewContact() {
-  return 'newContact';
+  return <PageHeader title="Novo contato" />;
 }
